c676e6edd7805a1218ddb7135161b5c6b1459834
Ricardo J. Vazquez <rix501@gmail.com>
2011-09-01 14:47:19 -0400

    beauty

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -23,11 +23,23 @@
             template: _.template($("#container-entrar-template").html()),
             tagName: 'div',
             className: 'container',
+            
+            events: {
+                'click .add-on :checkbox' : 'togglePreCheck'
+            },
 
             initialize: function() {
                 _.bindAll(this, 'render');
                 $('li.active').removeClass('active');
                 $('li.entrar').addClass('active');
+            },
+            
+            togglePreCheck: function(){
+                if ($('.add-on :checkbox').attr('checked')) {
+                    $('.add-on :checkbox').parents('.add-on').addClass('active');
+                } else {
+                    $('.add-on :checkbox').parents('.add-on').removeClass('active');
+                }
             },
 
             render: function() {
@@ -41,34 +53,15 @@
             }
         });
         
-        window.FormAddView = Backbone.View.extend({
-            template: _.template($("#form-add-template").html()),
-            tagName: 'form',
-
-            initialize: function() {
+        window.ContainerBuscarView = ContainerEntrarView.extend({
+           template:  _.template($("#container-buscar-template").html()),
+           initialize: function() {
                 _.bindAll(this, 'render');
+                $('li.active').removeClass('active');
+                $('li.buscar').addClass('active');
             },
-
-            render: function() {
-                $(this.el).html(this.template());
-                return this;
-            }
         });
-        
-        window.FormRemoveView = Backbone.View.extend({
-            template: _.template($("#form-remove-template").html()),
-            tagName: 'form',
-
-            initialize: function() {
-                _.bindAll(this, 'render');
-            },
-
-            render: function() {
-                $(this.el).html(this.template());
-                return this;
-            }
-        });
-        
+                
         window.ContainerTablesView = Backbone.View.extend({
             template: _.template($("#container-tables-template").html()),
             tagName: 'div',
@@ -93,7 +86,7 @@
             routes: {
                 '': 'entrar',
                 '/': 'entrar',
-                '/buscar': 'table',
+                '/buscar': 'buscar',
                 '/demandas': 'table',
                 '/informes': 'table',
                 '/actualizar': 'table',
@@ -111,6 +104,12 @@
                 $('#content').append(this.containerEntrarView.render().el);
             },
             
+            buscar: function() {
+                this.containerBuscarView = new ContainerBuscarView();
+                $('#content').empty();
+                $('#content').append(this.containerBuscarView.render().el);
+            },
+            
             table: function(){                
                 this.containerTablesView = new ContainerTablesView();
                 $('#content').empty();
@@ -121,6 +120,7 @@
         // Kick off the application
         window.App = new Housing();
         Backbone.history.start();
+        
     });
 
 })(jQuery);
