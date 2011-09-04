86b3cbe83016e31bf53918da82e3919540d52dd4
Ricardo J. Vazquez <rix501@gmail.com>
2011-09-04 14:18:44 -0400

    pdfkit

--- server.js
+++ server.js
@@ -29,5 +29,17 @@ app.get('/', function(req, res){
   res.sendfile('/public/index.html');
 });
 
+app.get('/pdf', function(req, res){
+	var PDFDocument = require('pdfkit');
+    var doc = new PDFDocument({size: 'legal',layout: 'landscape'});
+    
+    doc.text('This text is left aligned. ');
+    
+    doc.path('M 100,20 L 200,160 Q 230,00 250,120 C 290,-40 300,200 400,150 L 500,90')
+       .stroke();
+    
+    res.send(doc.output(),{'Content-type':'application/pdf'});
+});
+
 app.listen(process.env.PORT || 8001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
