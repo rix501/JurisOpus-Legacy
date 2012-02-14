2c0df4bfbad70c785e444d1ec4c303ba8bccef59
Ricardo J. Vazquez <rix501@gmail.com>
2012-02-13 22:10:59 -0400

    comma commit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.configure('production', function(){
 	var fs = require('fs');
 	var util = require('util');
 	var is = fs.createReadStream('/home/node/local/lib/node_modules/pdfkit/node_modules/flate/build/Release/zlib_bindings.node')
-	var os = fs.createWriteStream('./home/node/local/lib/node_modules/pdfkit/node_modules/flate/lib/zlib_bindings.node'); 
+	var os = fs.createWriteStream('/home/node/local/lib/node_modules/pdfkit/node_modules/flate/lib/zlib_bindings.node'); 
 	util.pump(is, os);
 });
 
