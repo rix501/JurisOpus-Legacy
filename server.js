682c0f27f5dc3532e158433e122be1b58cb20a65
Ricardo J. Vazquez <rix501@gmail.com>
2012-02-13 22:07:15 -0400

    comma commit

--- server.js
+++ server.js
@@ -7,20 +7,25 @@ var app = module.exports = express.createServer();
 process.env.NODE_ENV = app.settings.env;
 
 app.configure(function(){
-  app.use(express.bodyParser());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: "JurisSecret"}));
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: "JurisSecret"}));
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+	app.use(express.errorHandler()); 
+	var fs = require('fs');
+	var util = require('util');
+	var is = fs.createReadStream('/home/node/local/lib/node_modules/pdfkit/node_modules/flate/build/Release/zlib_bindings.node')
+	var os = fs.createWriteStream('./home/node/local/lib/node_modules/pdfkit/node_modules/flate/lib/zlib_bindings.node'); 
+	util.pump(is, os);
 });
 
 //My Libs
