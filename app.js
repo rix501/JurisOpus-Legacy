5e305d064c142d3b6f4fc9a715fb2bbfe9bf97d6
Ricardo J. Vazquez <rix501@gmail.com>
2012-02-16 21:55:54 -0400

    cmn

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
-var http = require('http'); 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(3000);
+//system and npm libs
+var express = require('express');
+var fs = require('fs');
+
+// Configuration
+var app = module.exports = express.createServer();
+process.env.NODE_ENV = app.settings.env;
+
+app.configure(function(){
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: "JurisSecret"}));
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+	app.use(express.errorHandler()); 
+});
+
+//My Libs
+var routes = require('./modules/routes');
+var Models = require('./modules/models');
+
+ //"express":">=2.5.8",
+    //"mysql":">=0.9.5"
+    //"pdfkit":">=0.1.6",
+ 
+// Routes
+routes.setup(app, Models);
+
+app.listen(process.env.C9_PORT || process.env.PORT || 3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
