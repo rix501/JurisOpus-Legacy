e6a404eac22510ffd6c3a01e20da40a0ca02bdf9
Ricardo J. Vazquez <rix501@gmail.com>
2012-04-04 10:17:21 -0400

    testing pdfkit 0.1.7

--- modules/models/casos.js
+++ modules/models/casos.js
@@ -3,7 +3,7 @@ var _ = require('underscore');
 var client = require('../db');
 var queries = require('../queries');
 var utils = require('../utils');
-if(process.env.NODE_ENV == 'development')
+//if(process.env.NODE_ENV == 'development')
     var pdfFactory = require('../pdf/PDFFactory');
 
 module.exports = function(Backbone, Models) {
